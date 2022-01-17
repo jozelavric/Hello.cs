c9eac33a601fac70113173fc61a55ee3cf460270
jozelavric <97885332+jozelavric@users.noreply.github.com>
2022-01-17 12:09:46 +0100

    Update Hello.cs

--- Hello.cs
+++ Hello.cs
@@ -3,5 +3,10 @@ public class Hello
    public static void Main()
    {
       System.Console.WriteLine("Hello, World!");
+<<<<<<< HEAD
+      System.Console.WriteLine("Hello, Local!");
+=======
+      System.Console.WriteLine("Hello, GitHub!");
+>>>>>>> db3fea923962f58c0160a35031f6b21df44c07fe
    }
 }
